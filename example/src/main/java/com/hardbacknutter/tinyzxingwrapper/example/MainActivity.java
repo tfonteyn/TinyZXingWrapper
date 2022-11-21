56b18a646532e5cd9dea9f403c69498fb1967264
tfonteyn <user@localhost>
2022-11-21 13:04:09 +0100

    updated

--- example/src/main/java/com/hardbacknutter/tinyzxingwrapper/example/MainActivity.java
+++ example/src/main/java/com/hardbacknutter/tinyzxingwrapper/example/MainActivity.java
@@ -5,15 +5,15 @@ import android.view.View;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.CameraSelector;
 
-import com.google.android.material.snackbar.Snackbar;
 import com.hardbacknutter.tinyzxingwrapper.ScanContract;
 import com.hardbacknutter.tinyzxingwrapper.ScanIntentResult;
 import com.hardbacknutter.tinyzxingwrapper.ScanOptions;
-import com.hardbacknutter.tinyzxingwrapper.scanner.DecoderType;
 import com.hardbacknutter.tinyzxingwrapper.example.databinding.ActivityMainBinding;
+import com.hardbacknutter.tinyzxingwrapper.scanner.DecoderType;
 
 public class MainActivity
         extends AppCompatActivity {
@@ -21,40 +21,40 @@ public class MainActivity
     private ActivityMainBinding vb;
 
     private final ActivityResultLauncher<ScanContract.Input> barcodeLauncher =
-            registerForActivityResult(new ScanContract(), o -> o.ifPresent(result -> {
-                if (result.isSuccess() && result.getText() != null) {
-                    Snackbar.make(vb.getRoot(),
-                            getString(R.string.msg_barcode, result.getText()),
-                            Snackbar.LENGTH_LONG).show();
+            registerForActivityResult(new ScanContract(), result -> {
+                if (result.isSuccess()) {
+                    final String text = getString(R.string.msg_barcode,
+                                                  result.getText(),
+                                                  String.valueOf(result.getFormat()));
+                    vb.lastScan.setText(text);
 
                 } else {
-                    switch (result.getFailure()) {
-                        case ScanIntentResult.Failure.REASON_MISSING_CAMERA_PERMISSION:
-                            Snackbar.make(vb.getRoot(),
-                                    R.string.err_permission,
-                                    Snackbar.LENGTH_LONG).show();
-                            break;
-                        case ScanIntentResult.Failure.REASON_TIMEOUT:
-                            Snackbar.make(vb.getRoot(),
-                                    R.string.err_hard_timeout,
-                                    Snackbar.LENGTH_LONG).show();
-                            break;
-                        case ScanIntentResult.Failure.REASON_INACTIVITY:
-                            Snackbar.make(vb.getRoot(),
-                                    R.string.err_inactivity,
-                                    Snackbar.LENGTH_LONG).show();
-                            break;
-                        default:
-                            Snackbar.make(vb.getRoot(),
-                                    getString(R.string.err_unknown, result.getFailure()),
-                                    Snackbar.LENGTH_LONG).show();
-                            break;
+                    final String reason = result.getFailure();
+                    if (reason != null) {
+                        switch (reason) {
+                            case ScanIntentResult.Failure.REASON_MISSING_CAMERA_PERMISSION:
+                                vb.lastScan.setText(R.string.err_permission);
+                                break;
+                            case ScanIntentResult.Failure.REASON_TIMEOUT:
+                                vb.lastScan.setText(R.string.err_hard_timeout);
+                                break;
+                            case ScanIntentResult.Failure.REASON_INACTIVITY:
+                                vb.lastScan.setText(R.string.err_inactivity);
+                                break;
+                            default:
+                                vb.lastScan.setText(
+                                        getString(R.string.err_unknown, reason));
+                                break;
+                        }
+                    } else {
+                        vb.lastScan.setText(
+                                getString(R.string.err_unknown, "???"));
                     }
                 }
-            }));
+            });
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         vb = ActivityMainBinding.inflate(getLayoutInflater());
@@ -65,17 +65,17 @@ public class MainActivity
         vb.frontCamera.setOnClickListener(this::scanBarcodeFrontCamera);
     }
 
-    public void scanBarcode(@NonNull final View view) {
+    private void scanBarcode(@NonNull final View view) {
         barcodeLauncher.launch(new ScanOptions());
     }
 
-    public void scanBarcodeInverted(@NonNull final View view) {
+    private void scanBarcodeInverted(@NonNull final View view) {
         final ScanOptions options = new ScanOptions();
         options.setDecoderType(DecoderType.Inverted);
         barcodeLauncher.launch(options);
     }
 
-    public void scanBarcodeFrontCamera(@NonNull final View view) {
+    private void scanBarcodeFrontCamera(@NonNull final View view) {
         final ScanOptions options = new ScanOptions();
         options.setPrompt(getString(R.string.msg_scan_prompt));
         options.setUseCameraWithLensFacing(CameraSelector.LENS_FACING_FRONT);
